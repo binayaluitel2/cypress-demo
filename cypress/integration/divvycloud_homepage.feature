35edde0c2a13734b58113ed1a02449fc864ae217
binayaluitel2 <binayababuluitel@gmail.com>
2021-01-22 00:48:00 -0500

    Added tests for top navbar and main navbar

--- cypress/integration/divvycloud_homepage.feature
+++ cypress/integration/divvycloud_homepage.feature
@@ -15,3 +15,13 @@ Feature: DivvyCloud Homepage
         Examples:
                   | bannerText                                     |
                   | Which cloud security solution is right for you |
+
+        Scenario: Top header navbar
+            Given I go to DivvyCloud page
+             Then I should see page title as "DivvyCloud - Protecting Cloud & Container Environments"
+              And I should see "docs","support","pricing" and "request_a_demo"
+
+        Scenario: Main header navbar
+            Given I go to DivvyCloud page
+             Then I should see page title as "DivvyCloud - Protecting Cloud & Container Environments"
+              And I should see "divvy_logo","why_divvy","platform", "solutions","customers","resources" and "company"
--- cypress/integration/divvycloud_homepage/divvycloud_homepage.js
+++ cypress/integration/divvycloud_homepage/divvycloud_homepage.js
@@ -46,3 +46,48 @@ Then(`Banner text should contain {string}`, (bannerText) => {
     });
   cy.go("back");
 });
+
+And(
+  `I should see {string},{string},{string} and {string}`,
+  (docs, support, pricing, request_a_demo) => {
+    var docs = cy.get("#menu-item-173 > a");
+    var support = cy.get("#menu-item-174");
+    var pricing = cy.get("#menu-item-16434 > a");
+    var request_a_demo = cy.get("#menu-item-175 > a");
+
+    docs.exist;
+    support.exist;
+    pricing.exist;
+    request_a_demo.exist;
+    
+  }
+);
+
+And(
+  `I should see {string},{string},{string}, {string},{string},{string} and {string}`,
+  (
+    divvy_logo,
+    why_divvy,
+    platform,
+    solutions,
+    customers,
+    resources,
+    company
+  ) => {
+    var divvy_logo = cy.get("#svg-replaced-4");
+    var why_divvy = cy.get("#menu-item-167");
+    var platform = cy.get("#menu-item-168");
+    var solutions = cy.get("#menu-item-169");
+    var customers = cy.get("#menu-item-170");
+    var resources = cy.get("#menu-item-171");
+    var company = cy.get("#menu-item-172");
+
+    divvy_logo.exist;
+    why_divvy.exist;
+    platform.exist;
+    solutions.exist;
+    customers.exist;
+    resources.exist;
+    company.exist;
+  }
+);
